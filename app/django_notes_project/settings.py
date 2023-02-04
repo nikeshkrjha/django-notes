8e9ac8a45238b268283662a102b7735470d489db
Nikesh Jha <nikeshkrjha@gmail.com>
2023-02-04 16:57:50 -0500

    Serving media files using cloudinary

--- app/django_notes_project/settings.py
+++ app/django_notes_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 import environ
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 
 env = environ.Env()
 environ.Env.read_env()
@@ -43,7 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'notes',
-    'rest_framework.authtoken'
+    'rest_framework.authtoken',
+    'cloudinary'
 ]
 
 MIDDLEWARE = [
@@ -123,7 +127,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
@@ -134,3 +142,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Setting custom user model
 AUTH_USER_MODEL = 'notes.CustomUser'
+
+cloudinary.config(
+  cloud_name = env("CLOUD_NAME"),
+  api_key = env("CLOUDINARY_API_KEY"),
+  api_secret = env("CLOUDINARY_API_SECRET")
+)
